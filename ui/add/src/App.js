e9ddbc360eb5a56e498ef4be3036602f08c51eba
mchp <piao.maria@gmail.com>
2020-01-02 19:00:52 -0800

    fix error message on add view

--- ui/add/src/App.js
+++ ui/add/src/App.js
@@ -5,18 +5,18 @@ class add extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      vocab: "bonita",
-      translations: [{translation:"pretty", class:"adjective"}, {translation:"beautiful", class:"adjective"}],
+      vocab: "",
+      translations: [],
       selected: "",
       loading: false,
-      error: false,
+      error: "",
     }
     this.lookup = this.lookup.bind(this);
     this.submit = this.submit.bind(this);
   }
   
   lookup() {
-    this.setState({loading: "true"});
+    this.setState({loading: "true", error: ""});
     var vocab = document.getElementById("vocab").value;
     fetch("/lookup?vocab=" + vocab)
       .then(res => res.json())
@@ -24,8 +24,9 @@ class add extends Component {
         this.setState({
           loading: false,
           vocab: vocab,
-          translations: result,
+          translations: result ? result : [],
           selected: "",
+          error: result ? "" : "No result found",
         });
       });
   }
@@ -41,10 +42,11 @@ class add extends Component {
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         body: 'vocab=' + this.state.vocab + '&translation=' + translation,
-      }).then(
-        () => {this.setState({loading: false})},
-        (error) => {this.setState({loading:false, error : error.message})}
-      )
+      })
+        .then(res => res.text())
+        .then(
+          (m) => {this.setState({loading:false, error : m})}
+        )
   }};
   
   render() {
@@ -65,6 +67,10 @@ class add extends Component {
           </div>
           <button onClick={this.submit}>submit</button>
         </div>
+        <div className={this.state.loading?"":"hidden"}>
+          loading...
+        </div>
+        {this.state.error}
       </div>
     );
   }
